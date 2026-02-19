caefbb59d2536ec7c476d1a8f8ed5ddf313e26bd
Denis Ivanov <denisvictorovichivanov@gmail.com>
2026-02-19 12:11:17 +0200

    Enable AddYears test

--- src/DuckDB.EFCore/DuckDBMethods.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-namespace DuckDB.EFCore;
-
-internal static class DuckDBMethods
-{
-    public static readonly MethodInfo DateOnlyAddDays = typeof(DateOnly).GetMethod(nameof(DateOnly.AddDays), [typeof(int)])!;
-}
