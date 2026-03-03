70926df85128ef04f25ab62801144e658dd424df
Denis Ivanov <denisvictorovichivanov@gmail.com>
2026-03-03 21:33:07 +0200

    Add unnest expression

---
+++ src/DuckDB.EFCore/Extensions/Internal/DuckDBShapedQueryExpressionExtensions.cs
@@ -0,0 +1,190 @@
+﻿using DuckDB.EFCore.Query.Expressions.Internal;
+using DuckDB.EFCore.Storage.Internal;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+
+namespace DuckDB.EFCore.Extensions.Internal;
+
+public static class DuckDBShapedQueryExpressionExtensions
+{
+    public static bool TryExtractArray(
+        this ShapedQueryExpression source,
+        [NotNullWhen(true)] out SqlExpression? array,
+        bool ignoreOrderings = false,
+        bool ignorePredicate = false)
+        => TryExtractArray(source, out array, out _, ignoreOrderings, ignorePredicate);
+
+    public static bool TryExtractArray(
+        this ShapedQueryExpression source,
+        [NotNullWhen(true)] out SqlExpression? array,
+        [NotNullWhen(true)] out ColumnExpression? projectedColumn,
+        bool ignoreOrderings = false,
+        bool ignorePredicate = false)
+    {
+        if (source.QueryExpression is SelectExpression
+            {
+                Tables: [DuckDBUnnestExpression { Array: var a } unnest],
+                GroupBy: [],
+                Having: null,
+                IsDistinct: false,
+                Limit: null,
+                Offset: null
+            } select
+            && (ignorePredicate || select.Predicate is null)
+            && (ignoreOrderings
+                || select.Orderings is []
+                || (select.Orderings is [{ Expression: ColumnExpression { Name: "ordinality", TableAlias: var orderingTableAlias } }]
+                    && orderingTableAlias == unnest.Alias))
+            && IsDuckDBArray(a)
+            && TryGetProjectedColumn(source, out var column))
+        {
+            array = a;
+            projectedColumn = column;
+            return true;
+        }
+
+        array = null;
+        projectedColumn = null;
+        return false;
+    }
+
+    public static bool TryExtractJsonArray(
+        this ShapedQueryExpression source,
+        [NotNullWhen(true)] out SqlExpression? jsonArray,
+        [NotNullWhen(true)] out SqlExpression? projectedElement,
+        out bool isElementNullable,
+        bool ignoreOrderings = false,
+        bool ignorePredicate = false)
+    {
+        if (source.QueryExpression is SelectExpression
+            {
+                Tables:
+                [
+                    TableValuedFunctionExpression
+                {
+                    Name: "jsonb_array_elements_text" or "json_array_elements_text",
+                    Arguments: [var json]
+                } tvf
+                ],
+                GroupBy: [],
+                Having: null,
+                IsDistinct: false,
+                Limit: null,
+                Offset: null
+            } select
+            && (ignorePredicate || select.Predicate is null)
+            && (ignoreOrderings
+                || select.Orderings is []
+                || (select.Orderings is [{ Expression: ColumnExpression { Name: "ordinality", TableAlias: var orderingTableAlias } }]
+                    && orderingTableAlias == tvf.Alias))
+            && TryGetScalarProjection(source, out var projectedScalar))
+        {
+            jsonArray = json;
+
+            switch (projectedScalar)
+            {
+                case SqlUnaryExpression
+                {
+                    OperatorType: ExpressionType.Convert,
+                    Operand: ColumnExpression { IsNullable: var isNullable }
+                } convert:
+                    projectedElement = convert;
+                    isElementNullable = isNullable;
+                    return true;
+                case ColumnExpression { IsNullable: var isNullable } column:
+                    projectedElement = column;
+                    isElementNullable = isNullable;
+                    return true;
+                default:
+                    throw new UnreachableException();
+            }
+        }
+
+        jsonArray = null;
+        projectedElement = null;
+        isElementNullable = false;
+        return false;
+    }
+
+    public static bool TryConvertToArray(
+        this ShapedQueryExpression source,
+        [NotNullWhen(true)] out SqlExpression? array,
+        bool ignoreOrderings = false,
+        bool ignorePredicate = false)
+    {
+        if (source.QueryExpression is SelectExpression
+            {
+                Tables: [ValuesExpression { ColumnNames: ["_ord", "Value"], RowValues.Count: > 0 } valuesExpression],
+                GroupBy: [],
+                Having: null,
+                IsDistinct: false,
+                Limit: null,
+                Offset: null
+            } select
+            && (ignorePredicate || select.Predicate is null)
+            && (ignoreOrderings || select.Orderings is []))
+        {
+            var elements = new SqlExpression[valuesExpression.RowValues.Count];
+
+            for (var i = 0; i < elements.Length; i++)
+            {
+                elements[i] = valuesExpression.RowValues[i].Values[1];
+            }
+
+            array = new DuckDBNewArrayExpression(elements, valuesExpression.RowValues[0].Values[1].Type.MakeArrayType(), typeMapping: null);
+            return true;
+        }
+
+        array = null;
+        return false;
+    }
+
+    private static bool IsDuckDBArray(SqlExpression expression)
+        => expression switch
+        {
+            { TypeMapping: DuckDBArrayTypeMapping } => true,
+            { TypeMapping: DuckDBJsonTypeMapping } => false,
+            _ => true
+        };
+
+    private static bool TryGetProjectedColumn(
+        ShapedQueryExpression shapedQueryExpression,
+        [NotNullWhen(true)] out ColumnExpression? projectedColumn)
+    {
+        if (TryGetScalarProjection(shapedQueryExpression, out var scalar) && scalar is ColumnExpression column)
+        {
+            projectedColumn = column;
+            return true;
+        }
+
+        projectedColumn = null;
+        return false;
+    }
+
+    private static bool TryGetScalarProjection(
+        ShapedQueryExpression shapedQueryExpression,
+        [NotNullWhen(true)] out SqlExpression? projectedScalar)
+    {
+        var shaperExpression = shapedQueryExpression.ShaperExpression;
+        if (shaperExpression is UnaryExpression { NodeType: ExpressionType.Convert } unaryExpression
+            && unaryExpression.Operand.Type.IsNullableType()
+            && unaryExpression.Operand.Type.UnwrapNullableType() == unaryExpression.Type)
+        {
+            shaperExpression = unaryExpression.Operand;
+        }
+
+        if (shaperExpression is ProjectionBindingExpression projectionBindingExpression
+            && shapedQueryExpression.QueryExpression is SelectExpression selectExpression
+            && selectExpression.GetProjection(projectionBindingExpression) is SqlExpression scalar)
+        {
+            projectedScalar = scalar;
+            return true;
+        }
+
+        projectedScalar = null;
+        return false;
+    }
+}
---
+++ src/DuckDB.EFCore/Query/Expressions/Internal/DuckDBUnnestExpression.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using System.Linq.Expressions;
+
+namespace DuckDB.EFCore.Query.Expressions.Internal;
+
+public class DuckDBUnnestExpression : DuckDBTableValuedFunctionExpression
+{
+    public virtual SqlExpression Array => Arguments[0];
+
+    public virtual string ColumnName => ColumnInfos![0].Name;
+
+    public DuckDBUnnestExpression(string alias, SqlExpression array, string columnName, bool withOrdinality = true)
+        : this(alias, array, new ColumnInfo(columnName), withOrdinality)
+    {
+    }
+
+    private DuckDBUnnestExpression(string alias, SqlExpression array, ColumnInfo? columnInfo,
+        bool withOrdinality = true)
+        : base(alias, "unnest", [array], columnInfo is null ? null : [columnInfo.Value], withOrdinality)
+    {
+    }
+
+    protected override Expression VisitChildren(ExpressionVisitor visitor)
+    {
+        return visitor.Visit(Array) is var visitedArray && visitedArray == Array
+            ? this
+            : new DuckDBUnnestExpression(Alias, (SqlExpression)visitedArray, ColumnName, WithOrdinality);
+    }
+
+    public override TableValuedFunctionExpression Update(IReadOnlyList<SqlExpression> arguments)
+    {
+        return arguments is [var singleArgument]
+            ? Update(singleArgument)
+            : throw new ArgumentException();
+    }
+
+    public virtual DuckDBUnnestExpression Update(SqlExpression array)
+    {
+        return array == Array
+            ? this
+            : new DuckDBUnnestExpression(Alias, array, ColumnName, WithOrdinality);
+    }
+
+    public override TableExpressionBase Clone(string? alias, ExpressionVisitor cloningExpressionVisitor)
+    {
+        return new DuckDBUnnestExpression(alias!, (SqlExpression)cloningExpressionVisitor.Visit(Array), ColumnName, WithOrdinality);
+    }
+
+    public override TableValuedFunctionExpression WithAlias(string newAlias)
+    {
+        return new DuckDBUnnestExpression(newAlias, Array, ColumnName, WithOrdinality);
+    }
+
+    public override DuckDBTableValuedFunctionExpression WithColumnInfos(IReadOnlyList<ColumnInfo>? columnInfos)
+    {
+        return new DuckDBUnnestExpression(Alias,
+            Array,
+            columnInfos switch
+            {
+                [] => null,
+                [var columnInfo] => columnInfo,
+                _ => throw new ArgumentException()
+            },
+            WithOrdinality);
+    }
+}
