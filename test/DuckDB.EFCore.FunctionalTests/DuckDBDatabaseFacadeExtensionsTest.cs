f3b66dabee174600c8d44755fb2bb391eef20d9a
Denis Ivanov <denisvictorovichivanov@gmail.com>
2026-04-13 22:26:11 +0300

    Add DuckDB provider checks to DatabaseFacade and MigrationBuilder extensions

---
+++ test/DuckDB.EFCore.FunctionalTests/DuckDBDatabaseFacadeExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿using DuckDB.EFCore.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace DuckDB.EFCore.FunctionalTests;
+
+public class DuckDBDatabaseFacadeExtensionsTest
+{
+    [ConditionalFact]
+    public void IsDuckDB_when_using_DuckDB()
+    {
+        using var context = new ProviderContext(
+            new DbContextOptionsBuilder()
+                .UseInternalServiceProvider(
+                    new ServiceCollection()
+                        .AddEntityFrameworkDuckDB()
+                        .BuildServiceProvider(validateScopes: true))
+                .UseDuckDB("Database=Maltesers").Options);
+        Assert.True(context.Database.IsDuckDB());
+    }
+
+    private class ProviderContext(DbContextOptions options) : DbContext(options);
+}
