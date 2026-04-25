4aac7115aef53ac457631211aa295bf7df2e963b
Denis Ivanov <denisvictorovichivanov@gmail.com>
2026-04-25 16:09:55 +0300

    Enable `NorthwindIncludeNoTrackingQueryDuckDBTest` tests

--- test/DuckDB.EFCore.FunctionalTests/Query/NorthwindIncludeNoTrackingQueryDuckDBTest.cs
+++ test/DuckDB.EFCore.FunctionalTests/Query/NorthwindIncludeNoTrackingQueryDuckDBTest.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore.Query;
+﻿using DuckDB.EFCore.FunctionalTests;
+using DuckDB.EFCore.FunctionalTests.Query;
 using Microsoft.EntityFrameworkCore.TestUtilities;
 using Xunit;
 
-namespace DuckDB.EFCore.FunctionalTests.Query;
+namespace Microsoft.EntityFrameworkCore.Query;
 
 public class NorthwindIncludeNoTrackingQueryDuckDBTest : NorthwindIncludeNoTrackingQueryTestBase<NorthwindQueryDuckDBFixture<NoopModelCustomizer>>
 {
@@ -11,44 +12,8 @@ public class NorthwindIncludeNoTrackingQueryDuckDBTest : NorthwindIncludeNoTrack
     }
 
     [ConditionalTheory(Skip = DuckDBSkipReasons.Tbd)]
-    public override Task Filtered_include_with_multiple_ordering(bool async)
-    {
-        return base.Filtered_include_with_multiple_ordering(async);
-    }
-
-    [ConditionalTheory(Skip = DuckDBSkipReasons.Tbd)]
-    public override Task Include_collection_with_cross_apply_with_filter(bool async)
-    {
-        return base.Include_collection_with_cross_apply_with_filter(async);
-    }
-
-    [ConditionalTheory(Skip = DuckDBSkipReasons.Tbd)]
     public override Task Include_collection_with_last_no_orderby(bool async)
     {
         return base.Include_collection_with_last_no_orderby(async);
-    }
-
-    [ConditionalTheory(Skip = DuckDBSkipReasons.Tbd)]
-    public override Task Include_collection_with_multiple_conditional_order_by(bool async)
-    {
-        return base.Include_collection_with_multiple_conditional_order_by(async);
-    }
-
-    [ConditionalTheory(Skip = DuckDBSkipReasons.Tbd)]
-    public override Task Include_collection_with_outer_apply_with_filter(bool async)
-    {
-        return base.Include_collection_with_outer_apply_with_filter(async);
-    }
-
-    [ConditionalTheory(Skip = DuckDBSkipReasons.Tbd)]
-    public override Task Include_collection_with_outer_apply_with_filter_non_equality(bool async)
-    {
-        return base.Include_collection_with_outer_apply_with_filter_non_equality(async);
-    }
-
-    [ConditionalTheory(Skip = DuckDBSkipReasons.Tbd)]
-    public override Task Repro9735(bool async)
-    {
-        return base.Repro9735(async);
     }
 }
