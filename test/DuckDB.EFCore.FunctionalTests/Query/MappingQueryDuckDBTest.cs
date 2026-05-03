20e148f42171fb9c35100570c653244309924764
Denis Ivanov <denisvictorovichivanov@gmail.com>
2026-05-04 02:39:09 +0300

    Move query test classes to Microsoft.EntityFrameworkCore.Query namespace

--- test/DuckDB.EFCore.FunctionalTests/Query/MappingQueryDuckDBTest.cs
+++ test/DuckDB.EFCore.FunctionalTests/Query/MappingQueryDuckDBTest.cs
@@ -1,11 +1,10 @@
-﻿using DuckDB.EFCore.FunctionalTests.TestUtilities;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Query;
+﻿using DuckDB.EFCore.FunctionalTests;
+using DuckDB.EFCore.FunctionalTests.TestUtilities;
 using Microsoft.EntityFrameworkCore.TestUtilities;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace DuckDB.EFCore.FunctionalTests.Query;
+namespace Microsoft.EntityFrameworkCore.Query;
 
 public class MappingQueryDuckDBTest : MappingQueryTestBase<MappingQueryDuckDBTest.MappingQueryDuckDBFixture>
 {
--- test/DuckDB.EFCore.FunctionalTests/Query/NonSharedPrimitiveCollectionsQueryDuckDBTest.cs
+++ test/DuckDB.EFCore.FunctionalTests/Query/NonSharedPrimitiveCollectionsQueryDuckDBTest.cs
@@ -1,11 +1,10 @@
-﻿using DuckDB.EFCore.FunctionalTests.TestUtilities;
+﻿using DuckDB.EFCore.FunctionalTests;
+using DuckDB.EFCore.FunctionalTests.TestUtilities;
 using DuckDB.EFCore.Infrastructure;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.EntityFrameworkCore.TestUtilities;
 using Xunit;
 
-namespace DuckDB.EFCore.FunctionalTests.Query;
+namespace Microsoft.EntityFrameworkCore.Query;
 
 public class NonSharedPrimitiveCollectionsQueryDuckDBTest : NonSharedPrimitiveCollectionsQueryRelationalTestBase
 {
