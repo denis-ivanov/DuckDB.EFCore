ac79325dc81fd4c6eaa3cad56ac6884ad393130f
Daniel Gut <daniel.gut@outlook.com>
2026-05-08 14:03:08 +0200

    Update DuckDBEntityTypeExtensions.cs

--- src/DuckDB.EFCore/Extensions/DuckDBEntityTypeExtensions.cs
+++ src/DuckDB.EFCore/Extensions/DuckDBEntityTypeExtensions.cs
@@ -14,12 +14,6 @@ public static class DuckDBEntityTypeExtensions
     public static string? GetParquetPath(this IEntityType entityType)
         => entityType.FindAnnotation(DuckDBAnnotationNames.ParquetPath)?.Value as string;
 
-    public static string? GetParquetPathFactoryValue(this IEntityType entityType)
-        => entityType.FindAnnotation(DuckDBAnnotationNames.ParquetPathFactoryValue)?.Value as string;
-
-    public static Func<ServiceProvider, string>? GetParquetPathFactory(this IEntityType entityType)
-        => entityType.FindAnnotation(DuckDBAnnotationNames.ParquetPathFactory)?.Value as Func<ServiceProvider, string>;
-
     public static EntityTypeBuilder<TEntity> FromParquet<TEntity>(
         this EntityTypeBuilder<TEntity> entityTypeBuilder,
         string path)
@@ -28,26 +22,6 @@ public static class DuckDBEntityTypeExtensions
         ArgumentException.ThrowIfNullOrWhiteSpace(path);
 
         entityTypeBuilder.Metadata.SetAnnotation(DuckDBAnnotationNames.ParquetPath, path);
-        entityTypeBuilder.Metadata.RemoveAnnotation(DuckDBAnnotationNames.ParquetPathFactory);
-        entityTypeBuilder.Metadata.RemoveAnnotation(DuckDBAnnotationNames.ParquetPathFactoryValue);
-
-        return entityTypeBuilder;
-    }
-
-    public static EntityTypeBuilder<TEntity> FromParquet<TEntity>(
-        this EntityTypeBuilder<TEntity> entityTypeBuilder,
-        Func<ServiceProvider, string> parquetPathFactory)
-        where TEntity : class
-    {
-        ArgumentNullException.ThrowIfNull(parquetPathFactory);
-
-        var serviceProvider = new ServiceCollection().BuildServiceProvider();
-        var initialPath = parquetPathFactory(serviceProvider);
-        ArgumentException.ThrowIfNullOrWhiteSpace(initialPath);
-
-        entityTypeBuilder.Metadata.SetAnnotation(DuckDBAnnotationNames.ParquetPathFactory, parquetPathFactory);
-        entityTypeBuilder.Metadata.SetAnnotation(DuckDBAnnotationNames.ParquetPathFactoryValue, initialPath);
-        entityTypeBuilder.Metadata.RemoveAnnotation(DuckDBAnnotationNames.ParquetPath);
 
         return entityTypeBuilder;
     }
