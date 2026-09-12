8dde05028cfa1a8c67eb4d045723c71f58f15c02
Denis Ivanov <denisvictorovichivanov@gmail.com>
2026-09-12 21:14:25 +0300

    Add `to_base64(blob)` function support

--- src/DuckDB.EFCore/Query/ExpressionTranslators/Internal/DuckDBBlobMethodTranslator.cs
+++ src/DuckDB.EFCore/Query/ExpressionTranslators/Internal/DuckDBBlobMethodTranslator.cs
@@ -14,6 +14,21 @@ namespace DuckDB.EFCore.Query.ExpressionTranslators.Internal;
 /// </summary>
 public class DuckDBBlobMethodTranslator : IMethodCallTranslator
 {
+    private static readonly MethodInfo ToBase64String = typeof(Convert).GetRuntimeMethod(nameof(Convert.ToBase64String), [typeof(byte[])])!;
+
+    private readonly ISqlExpressionFactory _sqlExpressionFactory;
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    public DuckDBBlobMethodTranslator(ISqlExpressionFactory sqlExpressionFactory)
+    {
+        _sqlExpressionFactory = sqlExpressionFactory;
+    }
+
     /// <summary>
     ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
     ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
@@ -26,6 +41,16 @@ public class DuckDBBlobMethodTranslator : IMethodCallTranslator
         IReadOnlyList<SqlExpression> arguments,
         IDiagnosticsLogger<DbLoggerCategory.Query> logger)
     {
+        if (method == ToBase64String)
+        {
+            return _sqlExpressionFactory.Function(
+                "to_base64",
+                [arguments[0]],
+                nullable: true,
+                argumentsPropagateNullability: [true],
+                typeof(string));
+        }
+
         return null;
     }
 }
