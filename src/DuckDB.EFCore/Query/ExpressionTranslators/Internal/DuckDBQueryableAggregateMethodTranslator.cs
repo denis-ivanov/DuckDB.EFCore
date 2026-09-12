8bfed9c2bca8658991b4d6b7d1edb23f1ff9b09e
Denis Ivanov <denisvictorovichivanov@gmail.com>
2026-09-12 13:03:22 +0300

    Add `regr_slope(y, x)` function support

--- src/DuckDB.EFCore/Query/ExpressionTranslators/Internal/DuckDBQueryableAggregateMethodTranslator.cs
+++ src/DuckDB.EFCore/Query/ExpressionTranslators/Internal/DuckDBQueryableAggregateMethodTranslator.cs
@@ -391,8 +391,8 @@ public class DuckDBQueryableAggregateMethodTranslator : IAggregateMethodCallTran
                     DuckDBBitStringTypeMapping.Default);
         }
 
-        // Support CORR(y, x), COVAR_POP(y, x), COVAR_SAMP(y, x), REGR_AVGX(y, x), REGR_AVGY(y, x), REGR_COUNT(y, x), REGR_INTERCEPT(y, x) and REGR_R2(y, x) aggregate functions
-        // (e.g., g.Corr(e => e.Y, e => e.X), g.CovarPop(e => e.Y, e => e.X), g.CovarSamp(e => e.Y, e => e.X), g.RegrAvgx(e => e.Y, e => e.X), g.RegrAvgy(e => e.Y, e => e.X), g.RegrCount(e => e.Y, e => e.X), g.RegrIntercept(e => e.Y, e => e.X), g.RegrR2(e => e.Y, e => e.X))
+        // Support CORR(y, x), COVAR_POP(y, x), COVAR_SAMP(y, x), REGR_AVGX(y, x), REGR_AVGY(y, x), REGR_COUNT(y, x), REGR_INTERCEPT(y, x), REGR_R2(y, x) and REGR_SLOPE(y, x) aggregate functions
+        // (e.g., g.Corr(e => e.Y, e => e.X), g.CovarPop(e => e.Y, e => e.X), g.CovarSamp(e => e.Y, e => e.X), g.RegrAvgx(e => e.Y, e => e.X), g.RegrAvgy(e => e.Y, e => e.X), g.RegrCount(e => e.Y, e => e.X), g.RegrIntercept(e => e.Y, e => e.X), g.RegrR2(e => e.Y, e => e.X), g.RegrSlope(e => e.Y, e => e.X))
         if (method.DeclaringType == typeof(DuckDBGroupingExtensions)
             && (method.Name == nameof(DuckDBGroupingExtensions.CorrAggregate)
                 || method.Name == nameof(DuckDBGroupingExtensions.CovarPopAggregate)
@@ -401,7 +401,8 @@ public class DuckDBQueryableAggregateMethodTranslator : IAggregateMethodCallTran
                 || method.Name == nameof(DuckDBGroupingExtensions.RegrAvgyAggregate)
                 || method.Name == nameof(DuckDBGroupingExtensions.RegrCountAggregate)
                 || method.Name == nameof(DuckDBGroupingExtensions.RegrInterceptAggregate)
-                || method.Name == nameof(DuckDBGroupingExtensions.RegrR2Aggregate))
+                || method.Name == nameof(DuckDBGroupingExtensions.RegrR2Aggregate)
+                || method.Name == nameof(DuckDBGroupingExtensions.RegrSlopeAggregate))
             && source.Selector is DuckDBRowValueExpression { Values.Count: 2 } pairRowValue
             && !source.IsDistinct)
         {
@@ -423,7 +424,8 @@ public class DuckDBQueryableAggregateMethodTranslator : IAggregateMethodCallTran
                 nameof(DuckDBGroupingExtensions.RegrAvgyAggregate) => "REGR_AVGY",
                 nameof(DuckDBGroupingExtensions.RegrCountAggregate) => "REGR_COUNT",
                 nameof(DuckDBGroupingExtensions.RegrInterceptAggregate) => "REGR_INTERCEPT",
-                _ => "REGR_R2"
+                nameof(DuckDBGroupingExtensions.RegrR2Aggregate) => "REGR_R2",
+                _ => "REGR_SLOPE"
             };
 
             return _sqlExpressionFactory.Function(
