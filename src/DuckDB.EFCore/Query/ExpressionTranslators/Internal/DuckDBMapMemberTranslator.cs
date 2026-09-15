1aec250a8dbd397b11fd893f4297b2982b243c05
Denis Ivanov <denisvictorovichivanov@gmail.com>
2026-09-16 00:04:54 +0300

    Add `Dictionary<TKey, TValue>` -> `cardinality(map)` translation

---
+++ src/DuckDB.EFCore/Query/ExpressionTranslators/Internal/DuckDBMapMemberTranslator.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using System.Reflection;
+
+namespace DuckDB.EFCore.Query.ExpressionTranslators.Internal;
+
+/// <summary>
+///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+///     any release. You should only use it directly in your code with extreme caution and knowing that
+///     doing so can result in application failures when updating to a new Entity Framework Core release.
+/// </summary>
+public class DuckDBMapMemberTranslator : IMemberTranslator
+{
+    private readonly ISqlExpressionFactory _sqlExpressionFactory;
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    public DuckDBMapMemberTranslator(ISqlExpressionFactory sqlExpressionFactory)
+    {
+        _sqlExpressionFactory = sqlExpressionFactory;
+    }
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    public SqlExpression? Translate(SqlExpression? instance, MemberInfo member, Type returnType, IDiagnosticsLogger<DbLoggerCategory.Query> logger)
+    {
+        if (instance is not null &&
+            member.DeclaringType is not null &&
+            member.DeclaringType.IsGenericType &&
+            member.DeclaringType.GetGenericTypeDefinition() == typeof(Dictionary<,>) &&
+            member.Name == nameof(Dictionary<,>.Count))
+        {
+            return _sqlExpressionFactory.Function(
+                name: "cardinality",
+                arguments: [instance],
+                nullable: true,
+                argumentsPropagateNullability: [true],
+                returnType: returnType);
+        }
+
+        return null;
+    }
+}
