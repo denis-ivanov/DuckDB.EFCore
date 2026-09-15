38668a228a216f630a79775a8952032f2ee70ea9
Denis Ivanov <denisvictorovichivanov@gmail.com>
2026-09-16 00:19:27 +0300

    Add `map_contains(map, key)` function support

---
+++ src/DuckDB.EFCore/Query/ExpressionTranslators/Internal/DuckDBMapMethodTranslator.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using System.Reflection;
+
+namespace DuckDB.EFCore.Query.ExpressionTranslators.Internal;
+
+/// <summary>
+///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+///     any release. You should only use it directly in your code with extreme caution and knowing that
+///     doing so can result in application failures when updating to a new Entity Framework Core release.
+/// </summary>
+public class DuckDBMapMethodTranslator : IMethodCallTranslator
+{
+    private readonly ISqlExpressionFactory _sqlExpressionFactory;
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    public DuckDBMapMethodTranslator(ISqlExpressionFactory sqlExpressionFactory)
+    {
+        _sqlExpressionFactory = sqlExpressionFactory;
+    }
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    public SqlExpression? Translate(
+        SqlExpression? instance,
+        MethodInfo method,
+        IReadOnlyList<SqlExpression> arguments,
+        IDiagnosticsLogger<DbLoggerCategory.Query> logger)
+    {
+        if (instance is not null &&
+            method.DeclaringType is not null &&
+            method.DeclaringType.IsGenericType &&
+            method.DeclaringType.GetGenericTypeDefinition() == typeof(Dictionary<,>) &&
+            method.Name == nameof(Dictionary<,>.ContainsKey))
+        {
+            return _sqlExpressionFactory.Function(
+                name: "map_contains",
+                arguments: [instance, arguments[0]],
+                nullable: true,
+                argumentsPropagateNullability: [true, true],
+                returnType: method.ReturnType);
+        }
+
+        return null;
+    }
+}
